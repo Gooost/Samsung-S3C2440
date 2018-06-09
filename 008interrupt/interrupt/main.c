1594bae5fe67b9a7cc5076d2a0c81d39f5686ed6
wang meng <462669174@qq.com>
2018-06-10 00:34:06 +0800

    add interrupt

---
+++ 008interrupt/interrupt/main.c
@@ -0,0 +1,50 @@
+/*
+ * main.c
+ *  
+ *  Created on: 2018.05.12
+ *      Author: wangmeng
+ */
+#include "S3C2440.h"
+#include "init.h"
+#include "led.h"
+#include "key.h"
+#include "uart.h"
+
+char g_charA = 'A';
+
+/**
+  * @brief  for a short delay. 
+  * @param  num: delay time.
+  * @retval None.
+  */
+void delay(volatile int num)
+{
+    while(num--);
+}
+/**
+  * @brief  main. 
+  * @param  None.
+  * @retval None.
+  */
+int main(void)
+{
+    led_io_init();
+    //key_io_init();
+    interrupt_init();
+	key_eint_init();
+    timer_init();
+
+    while(1)
+    {
+        putchar(g_charA);
+        g_charA++;
+        delay(100000);
+        if(g_charA>'Z')
+        {
+            g_charA = 'A';
+            puts("\r\n");     
+        }
+ 
+    }
+    return 0;
+}
