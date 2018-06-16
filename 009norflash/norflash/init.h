b225b82c8d8e09dd9d57aff1167d3bbe4ccc0dae
wang meng <462669174@qq.com>
2018-06-16 22:29:04 +0800

    add norflash

---
+++ 009norflash/norflash/init.h
@@ -0,0 +1,13 @@
+/*
+ * init.c
+ *  
+ *  Created on: 2018.05.12
+ *      Author: wangmeng
+ */
+#ifndef __INIT_H
+#define __INIT_H
+
+void sdram_init(void);
+int sdram_test(void);
+
+#endif // __INIT_H
---
+++ 009norflash/norflash/key.h
@@ -0,0 +1,13 @@
+/*
+ * key.c
+ *  
+ *  Created on: 2018.05.12
+ *      Author: wangmeng
+ */
+#ifndef __KEY_H
+#define __KEY_H
+
+void key_io_init(void);
+void key_scan(void);
+
+#endif // __KEY_H
